3ac3bec2a479e3dac73809d1a782a42df53422ec
Jorge Castillo <jorgecas60@gmail.com>
2022-06-29 02:13:00 -0400

    first commit

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
-<?php
-  ob_start();
-  require_once('includes/load.php');
-  if($session->isUserLoggedIn(true)) { redirect('home.php', false);}
-?>
+
 <?php include_once('layouts/header.php'); ?>
 <div class="login-page">
     <div class="text-center">
